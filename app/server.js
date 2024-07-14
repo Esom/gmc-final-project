bdd46d625e9791154475e539c909f74dd55b63f3
Ekeh Esom <mailesom@gmail.com>
2024-07-14 09:54:10 +0100

    add support for decryption

--- app/server.js
+++ app/server.js
@@ -20,11 +20,29 @@ function encrypt(text) {
   return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
+// Decrypt function
+function decrypt(text) {
+  const textParts = text.split(':');
+  const iv = Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
+
 // Route to encrypt text
 app.post('/encrypt', (req, res) => {
   const plaintext = req.body.text;
   const encryptedText = encrypt(plaintext);
   res.send({ encryptedText });
+});
+
+// Route to decrypt text
+app.post('/decrypt', (req, res) => {
+  const encryptedText = req.body.encrypted;
+  const decryptedText = decrypt(encryptedText);
+  res.send({ decryptedText });
 });
 
 app.listen(port, () => {
